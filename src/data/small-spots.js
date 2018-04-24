983dd2f6b0cd786858368f6755169b9f53b65088
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2018-04-24 23:40:45 +0300

    feat(app.css): styling

---
+++ src/data/small-spots.js
@@ -0,0 +1,53 @@
+import oddworld from '../assets/small-spots/oddworld.png'
+import deponia from '../assets/small-spots/deponia.png'
+import theSettlers2 from '../assets/small-spots/the-settlers-2.png'
+import neverwinterNights from '../assets/small-spots/neverwinter-nights.png'
+import assasinsCreed from '../assets/small-spots/assasins-creed.png'
+
+export const smallSpots = [
+    {
+        title: "ODDWORLD: STRANGER'S WRATH",
+        img: oddworld,
+        price: {
+            currency: 'USD',
+            value: 999,
+            minorUnits: 100
+        }
+    },
+    {
+        title: "CHAOS ON DEPONIA",
+        img: deponia,
+        price: {
+            currency: 'USD',
+            value: 999,
+            minorUnits: 100
+        }
+    },
+    {
+        title: "THE SETTLERS 2: GOLD EDITION",
+        img: theSettlers2,
+        price: {
+            currency: 'USD',
+            value: 599,
+            minorUnits: 100
+        }
+    },
+    {
+        title: "NEVERWINTER NIGHTS",
+        img: neverwinterNights,
+        price: {
+            currency: 'USD',
+            value: 499,
+            minorUnits: 100
+        }
+    },
+    {
+        title: "ASSASIN'S CREED: DIRECTOR'S CUT",
+        img: assasinsCreed,
+        price: {
+            currency: 'USD',
+            value: 999,
+            minorUnits: 100
+        }
+    }
+]
