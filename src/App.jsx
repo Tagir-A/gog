14aa6e8454bed65a0e81c2573275f8db47afc867
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2018-04-24 23:40:34 +0300

    feat(app.jsx): layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo-classic.svg';
+import cartIco from './assets/ico-cart.svg'
 import './App.css';
+import { smallSpots } from './data/small-spots';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="menu">
+          <div className="menu__container">
+            <a className='menu__logo'/>
+            <button className="cart-btn">
+              <img src={cartIco}/>
+            </button>
+          </div>
         </header>
         <div className="container">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="big-spot">
+        <h3 className="big-spot__title">GAME OF THE WEEK</h3>
+        <button className="big-spot__btn"></button>
+        </section>
+        <section className="small-spots">
+        {smallSpots.map(game => (
+          <div>
+          </div>
+        ))}
+        </section>
         </div>
 
       </div>
