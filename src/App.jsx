8b2b669d95b8918cb31979325f1eb544b745ace6
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2018-04-26 09:59:52 +0300

    feat(app.jsx): basic functionality WIP

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import './App.css';
 import { smallSpots } from './data/small-spots';
 
 class App extends Component {
+  state = {
+    cart: {
+      products: [],
+      isOpen: false
+    }
+  }
+
   render() {
+    const {cart:{isOpen, products}} = this.state
     return (
       <div className="app">
         <header className="menu">
           <div className="menu__container">
             <a className='menu__logo'/>
-            <button className="cart-btn">
+            <button
+            className="cart-btn"
+            onClick={this.handleCartClick}
+            >
               <img src={cartIco}/>
+              {products.length}
             </button>
+            { isOpen && this.renderCart() }
           </div>
         </header>
         <div className="container">
@@ -28,7 +41,10 @@ class App extends Component {
             <div className="small-spot__info">
               <h3 className="small-spot__info__title">{game.title}</h3>
               <div className="small-spot__btns">
-                <button className="small-spot__btn">
+                <button
+                className="small-spot__btn"
+                onClick={(e) => this.handleAddToCart(e,game)}
+                >
                 { game.price.value / game.price.minorUnits }
                 </button>
               </div>
@@ -37,10 +53,64 @@ class App extends Component {
         ))}
         </section>
         </div>
-
       </div>
     );
   }
+
+  renderCart() {
+    const {cart:{products}} = this.state
+    return (
+      <div className="cart">
+        <div className="cart__info">
+        <h4 className="cart__info__quantity">
+          {`${products.length} items in cart`}
+        </h4>
+        <h4 className="cart__info__price">
+          { products.reduce((totalPrice, item)=> totalPrice+= item.price.value / item.price.minorUnits, 0)}
+        </h4>
+        <button
+        className="cart__info__btn cart__info__btn--clear"
+        onClick={this.handleClearCart}
+        >
+          CLEAR CART
+        </button>
+        </div>
+        <div className="cart__content">
+          {products.map(item=> (
+            <h4 className="cart__content__game-title">{item.title}</h4>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  handleCartClick = () => {
+    const {cart} = this.state
+    const nextCart = {
+      ...cart,
+      isOpen: !cart.isOpen
+    }
+    this.setState({cart: nextCart})
+  }
+
+  handleAddToCart = (e,game) => {
+    const {cart} = this.state
+    const nextProducts = [...cart.products, game]
+    const nextCart = {
+      ...cart,
+      products: nextProducts
+    }
+    this.setState({cart: nextCart})
+  }
+
+  handleClearCart = () => {
+    const nextCart = {
+      ...this.state.cart,
+      products: []
+    }
+    this.setState({cart: nextCart})
+  }
+
 }
 
 export default App;
