c5dd1b9bd3190769ea76347c41d7a1b5e062b31b
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2018-05-06 14:38:58 +0300

    feat(tests): add image testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,17 +1,15 @@
-import {
-    configure,
-    shallow,
-    render,
-    mount
-} from "enzyme";
-import React from 'react'
+import { configure, shallow, render, mount } from "enzyme";
+import React from "react";
 import Adapter from "enzyme-adapter-react-16";
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+
+expect.extend({ toMatchImageSnapshot });
 
 configure({
-    adapter: new Adapter()
+  adapter: new Adapter()
 });
 
-global.React = React
-global.shallow = shallow
-global.render = render
-global.mount = mount
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
